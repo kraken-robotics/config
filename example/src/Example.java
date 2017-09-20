b215d7ddb62c912fcab7ad6e2add6d1a3285df2a
PF <pierre-francois.gimenez@irit.fr>
2017-09-20 11:32:09 +0200

    More detailed example

--- example/src/Example.java
+++ example/src/Example.java
@@ -1,7 +1,4 @@
-import java.util.HashMap;
-
 import pfg.config.Config;
-import pfg.config.ConfigInfo;
 
 /*
  * Copyright (C) 2013-2017 Pierre-François Gimenez
@@ -16,14 +13,38 @@ import pfg.config.ConfigInfo;
 
 public class Example
 {
-
 	public static void main(String[] args)
-	{		
-		/*
-		 * The config file is "config_example.ini"
-		 */
-		Config config = new Config(ConfigInfoExample.values(), "config_example.ini", "example", true);
-				
+	{
+		scenario1();
+		System.out.println("\n");
+		scenario2();
+		System.out.println("\n");
+		scenario3();
+	}
+	
+	/**
+	 * First scenario : we load the default values only
+	 */
+	public static void scenario1()
+	{
+		System.out.println("DEFAULT VALUES ONLY");
+		Config config;
+		config = new Config(ConfigInfoExample.values(), true);
+		
+		int valInteger = config.getInt(ConfigInfoExample.SOME_INTEGER_VALUE);
+		System.out.println("The default integer value : " + valInteger);
+	}
+	
+	/**
+	 * Second scenario : we load the config file "config_example.ini" with the profile "default"
+	 * You must specify at least one profile !
+	 */
+	public static void scenario2()
+	{
+		System.out.println("PROFILE : DEFAULT");
+
+		Config config = new Config(ConfigInfoExample.values(), true, "config_example.ini", "default");
+		
 		/*
 		 * There are several getters depending on the type of the variable
 		 */
@@ -32,20 +53,35 @@ public class Example
 
 		String valString = config.getString(ConfigInfoExample.SOME_STRING_VALUE);
 		System.out.println("The string value has been overridden with the config file : " + valString + " != " + ConfigInfoExample.SOME_STRING_VALUE.getDefaultValue());
-		
+
+		int valInteger = config.getInt(ConfigInfoExample.SOME_INTEGER_VALUE);
+		System.out.println("The integer value has been overridden with the config file : " + valInteger + " != " + ConfigInfoExample.SOME_INTEGER_VALUE.getDefaultValue());
+
 		boolean valBool = config.getBoolean(ConfigInfoExample.SOME_BOOLEAN_VALUE);
 		System.out.println("The boolean value has been overridden with the config file : " + valBool + " != " + ConfigInfoExample.SOME_BOOLEAN_VALUE.getDefaultValue());
 
 		/*
 		 * We override the value of "SOME_STRING_VALUE" with "override-value"
 		 */
-		HashMap<ConfigInfo, Object> override = new HashMap<ConfigInfo, Object>();
-		override.put(ConfigInfoExample.SOME_STRING_VALUE, "override-value");
-		config.override(override);
+		config.override(ConfigInfoExample.SOME_STRING_VALUE, "override-value");
 		
 		String newValString = config.getString(ConfigInfoExample.SOME_STRING_VALUE);
-		System.out.println("The string value has been overridden with the HashMap : " + newValString + " != " + valString);
+		System.out.println("The string value has been manually overridden : " + newValString + " != " + valString);		
+	}
+	
+	/**
+	 * Third scenario : we load the config file "config_example.ini" with two profiles "default" and "example"
+	 * (using more than two profiles is possible)
+	 * The order of the profiles is important ! The latter overrides the former.
+	 */
+	public static void scenario3()
+	{
+		System.out.println("PROFILE : DEFAULT + EXAMPLE");
 		
+		Config config = new Config(ConfigInfoExample.values(), true, "config_example.ini", "default", "example");
+		
+		int valInteger = config.getInt(ConfigInfoExample.SOME_INTEGER_VALUE);
+		System.out.println("The integer value has been overridden with the profile \"example\" : " + valInteger);
 	}
 
 }
